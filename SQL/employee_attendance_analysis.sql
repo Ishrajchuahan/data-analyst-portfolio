5cd2c945449dcb2912a23b0941a576355b04405d
Ishrajchuahan <162119969+Ishrajchuahan@users.noreply.github.com>
2026-01-30 13:15:37 +0530

    Added Employee Attendance Analysis project

---
+++ SQL/employee_attendance_analysis.sql
@@ -0,0 +1,8 @@
+/* 
+Project Name: Employee Attendance Analysis
+Domain: HR Analytics
+Tools Used: SQL Server
+Description:
+Analyzed employee attendance data to identify attendance patterns,
+leave trends, and employee regularity for management reporting.
+*/
