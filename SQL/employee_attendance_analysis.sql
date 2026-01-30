4e66c78192d17e839e96a094340ca0a69751211c
Ishrajchuahan <162119969+Ishrajchuahan@users.noreply.github.com>
2026-01-30 13:19:51 +0530

    Update employee_attendance_analysis.sql

--- SQL/employee_attendance_analysis.sql
+++ SQL/employee_attendance_analysis.sql
@@ -6,3 +6,13 @@ Description:
 Analyzed employee attendance data to identify attendance patterns,
 leave trends, and employee regularity for management reporting.
 */
+
+
+/*
+Business Problems:
+1. Calculate total working days, present days, and leave days per employee.
+2. Identify employees with attendance percentage below 75%.
+3. Identify employees with attendance above 90%.
+4. Analyze month-wise attendance trends.
+5. Detect employees with frequent absences.
+*/
